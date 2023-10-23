ddced95f3e480da67c9fb5a460b9e1f9001118fe
leesiro12 <phamducminh.1901@gmail.com>
2023-10-23 12:57:28 +0700

    1

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -7,34 +7,40 @@ public class Movement : MonoBehaviour
 {
     Rigidbody2D rb;
     public float jumpForce = 5f;
-    float playerDir = 0f;
-    PlayerActions playerActions;
+    Vector2 playerDir = Vector2.zero;
+    public ProjectFloraInputs playerController;
+    InputAction playerMove;
+    InputAction playerJump;
     public float movementSpeed = 3f;
 
     private void Awake()
     {
+        playerController = new ProjectFloraInputs();
         rb = GetComponent<Rigidbody2D>();
     }
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        playerActions = new PlayerActions();
-        playerActions.Enable();     //enabling input system
-        playerActions.Player.Move.performed += moving =>
-        {
+        playerMove = playerController.Player.Move;
+        playerMove.Enable();
 
-        };
-
+        playerJump = playerController.Player.Move;
+        playerJump.Enable();
+        playerJump.performed += Jump;
     }
-
+    private void OnDisable()
+    {
+        playerMove.Disable();
+        playerJump.Disable();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        playerDir = playerMove.ReadValue<Vector2>();
+        Move();
     }
     public void Move()
     {
-        rb.velocity = new Vector2(playerDir * movementSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(playerDir.x * movementSpeed, rb.velocity.y);
     }
     public void Jump(InputAction.CallbackContext context)
     {
